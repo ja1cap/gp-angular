981e11c72f277e9d6bed084d30432ffd1ef60320
ytsialitsyn <ytsialitsyn@scnsoft.com>
2016-02-03 16:34:59 +0300

    ver 2

--- MainController.js
+++ MainController.js
@@ -2,5 +2,9 @@
  * Created by TerDen on 03.02.2016.
  */
 app.controller('MainController', ['$scope', function ($scope) {
-   // так не работает $scope.summOfTwo = document.getElementById("number1") + document.getElementById("number");
+    $scope.summOfTwo = function(){
+      return document.getElementById("number1").value*1 + document.getElementById("number2").value*1;
+      // или можно так
+      //return $scope.number1 + $scope.number2
+    };
 }]);
