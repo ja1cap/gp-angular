2ed46d5db1b5f2e4edcdedca28ff9679e5b9c176
ytsialitsyn <ytsialitsyn@scnsoft.com>
2016-02-03 16:14:19 +0300

    Initial commit

---
+++ MainController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by TerDen on 03.02.2016.
+ */
+app.controller('MainController', ['$scope', function ($scope) {
+   // так не работает $scope.summOfTwo = document.getElementById("number1") + document.getElementById("number");
+}]);
---
+++ SummOfTwo.js
@@ -0,0 +1,4 @@
+/**
+ * Created by TerDen on 03.02.2016.
+ */
+var app = angular.module("TerDenApp", []);
